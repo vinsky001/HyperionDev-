6236fb2461e87e1a1b4bddcd07668e09d45d9c46
vinsky001 <ekibet544@gmail.com>
2024-02-14 23:36:57 +0300

    Implemented MongoDB connection setup and error handling in server.js

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, 'secret_key', (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
+export { verifyToken };
