4cbd3e57a06f3461cc36a61ffb060989bd2f59b2
vinsky001 <ekibet544@gmail.com>
2024-02-28 13:42:45 +0300

    Removed the mongodb connection to .env file and imported routes and initialized them for the app to run effectively

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from './server/routes/userRoutes.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
-
-const app = express(); 
 const PORT = process.env.PORT || 5000;
-
+const app = express(); 
 // Middleware
+app.use(cors());
 app.use(express.json());
+// use routes
+app.use('/api', userRoutes);
+
+// app.get('/test', (req, res) => {
+//   res.json({ message: 'Server is running' });
+// });
 
 // MongoDB Connection
-mongoose.connect('mongodb+srv://vinsky:<password>@cluster0.omftxzi.mongodb.net/', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => console.log('Connected to MongoDB'));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
