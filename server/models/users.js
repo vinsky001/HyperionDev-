b1643bd625ce7b47663ff777a67e3ecaa272671f
vinsky001 <ekibet544@gmail.com>
2024-02-28 13:48:23 +0300

    Removed username field from the user schema

--- server/models/users.js
+++ server/models/users.js
@@ -2,11 +2,6 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      trim: true,
-    },
     email: {
       type: String,
       required: true,
