c8721e89b3fcd4545a8d39fd520a01c4b4576525
vinsky001 <ekibet544@gmail.com>
2024-03-04 21:04:15 +0300

    Added dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,25 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/loginUser/loginUser';
 import Dashboard from './components/dashboard/dashboard';
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  // Function to handle authentication status
-  const handleAuthentication = (status) => {
-    setIsAuthenticated(status);
-  };
 
   return (
     <Router>
       <Routes>
         {/* Define route for Login */}
-        <Route path="/" element={<Login onAuthentication={handleAuthentication} />} />
-        
-        {/* route to dashboard */}
-        <Route
-          path="/dashboard"
-          element={isAuthenticated ? (
-            <>
-              <Navigate to="/dashboard/welcome" />
-              <Dashboard />
-            </>
-          ) : (
-            <Navigate to="/" />
-          )}
-        />        
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />       
         <Route
           path="/dashboard/welcome"
-          element={isAuthenticated ? <h1>Hello, you are logged in</h1> : <Navigate to="/" />}
+          element={Dashboard}
+        // element={isAuthenticated ? <h1>Hello, you are logged in</h1> : <Navigate to="/" />}
         />
       </Routes>
     </Router>
   );
 };
 
-export default App;
+export default App;
